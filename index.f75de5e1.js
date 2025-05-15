5eefc03c379c874787c52b7577e44158c271e1a6
z3rt <z3rt@MacBook-Pro-Aleksandr.local>
2025-05-15 20:08:44 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const inputs = [
+    ...document.querySelectorAll("input")
+];
+for (const input of inputs){
+    if (input.hasAttribute("id")) {
+        const label = document.createElement("label");
+        input.before(label);
+        label.setAttribute("for", `${input.id}`);
+        label.classList.add("field-label");
+        label.textContent = input.name;
+    }
+    if (input.hasAttribute("name")) input.setAttribute("placeholder", `${input.name.toUpperCase()}`);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
