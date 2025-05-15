85b41a6d82ccd1e95d3ca3f1dcf22d96bf06d88f
z3rt <z3rt@MacBook-Pro-Aleksandr.local>
2025-05-15 19:43:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,18 +2,19 @@
 
 const inputs = [...document.querySelectorAll('input')];
 
+
+
 for (const input of inputs) {
   const label = document.createElement('label');
 
-if (input.hasAttribute('id')) {
-  input.before(label);
-  label.setAttribute('for', `${input.id}`);
-  label.classList.add('field-label');
+  if (input.hasAttribute('id')) {
+    input.before(label);
+    label.setAttribute('for', `${input.id}`);
+    label.classList.add('field-label');
+    label.textContent = input.name;
+  }
+
+  if (input.hasAttribute('name')) {
+    input.setAttribute('placeholder', `${input.name.toUpperCase()}`);
 }
-
-if (input.hasAttribute('name')) {
-
-  label.textContent = input.name;
-  input.setAttribute('placeholder', `${input.name.toUpperCase()}`)
- }
 }
